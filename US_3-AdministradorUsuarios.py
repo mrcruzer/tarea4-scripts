4310db39914a1252f36111f123806064bfdbbd72
mrcruzer <isaacavelino14@gmail.com>
2022-08-14 21:48:51 -0400

    actualizando US 3 - Final

--- US_3-AdministradorUsuarios.py
+++ US_3-AdministradorUsuarios.py
@@ -40,20 +40,68 @@ class AdministradorUsuarios(unittest.TestCase):
         password = self.driver.find_element(By.ID,"systemUser_password").send_keys("CERtificado14")
         confirm_password = self.driver.find_element(By.ID,"systemUser_confirmPassword").send_keys("CERtificado14")
 
-        guardar = self.driver.find_element(By.ID,"btnSave").click()
+        guardar = self.driver.find_element(By.ID, "btnSave").click()
+
+        print("US 3 | TC01 - Validar Agregar Nuevo Usuario")
 
 
 
-   '''def test_buscar_usuario(self):
+    def test_buscar_usuario(self):
         usuario = self.driver.find_element(By.ID, 'txtUsername').send_keys("Admin")
-        password = self.driver.find_element(By.ID, 'txtPassword').send_keys("admin12345")
+        password = self.driver.find_element(By.ID, 'txtPassword').send_keys("admin123")
         login = self.driver.find_element(By.ID, "btnLogin").click()
         admin = self.driver.find_element(By.ID, "menu_admin_viewAdminModule").click()
 
+        search = self.driver.find_element(By.ID, "searchSystemUser_userName").send_keys("usuario1")
+        boton_search = self.driver.find_element(By.ID, "searchBtn").click()
+
+        print("US 3 | TC02 - Validar Buscar Usuario")
+
+    def test_editar_usuario(self):
+        usuario = self.driver.find_element(By.ID, 'txtUsername').send_keys("Admin")
+        password = self.driver.find_element(By.ID, 'txtPassword').send_keys("admin123")
+        login = self.driver.find_element(By.ID, "btnLogin").click()
+        admin = self.driver.find_element(By.ID, "menu_admin_viewAdminModule").click()
+
+        filas = len(self.driver.find_elements(By.XPATH, "/html/body/table/tbody/tr"))
+        columnas = len(self.driver.find_elements(By.XPATH, "/html/body/table/tbody/tr[1]/td"))
+
+        for f in range(2, filas + 1):
+            for c in range(1, columnas + 1):
+                valores = self.driver.find_element(By.XPATH, "/html/body/table/tbody/tr[" + str(f) + "]/td[" + str(c) + "]").text
+                self.assertEqual(valores, "usuario1").click()
+
+        title = self.driver.find_element(By.ID, "UserHeading")
+        if title.is_displayed():
+            botoncito = self.driver.find_element(By.ID, "btnSave")
+            botoncito.click()
+            usernamecito = self.driver.find_element(By.ID, "systemUser_userName").send_keys("usuario2")
+            botoncito.click()
 
 
-   def tearDown(self):
-        self.driver.quit()'''
+        print("US 3 | TC03 - Validar Editar Nuevo Usuario")
+
+    def test_borrar_usuario(self):
+        usuario = self.driver.find_element(By.ID, 'txtUsername').send_keys("Admin")
+        password = self.driver.find_element(By.ID, 'txtPassword').send_keys("admin123")
+        login = self.driver.find_element(By.ID, "btnLogin").click()
+        admin = self.driver.find_element(By.ID, "menu_admin_viewAdminModule").click()
+
+        filas = len(self.driver.find_elements(By.XPATH, "/html/body/table/tbody/tr"))
+        columnas = len(self.driver.find_elements(By.XPATH, "/html/body/table/tbody/tr[1]/td"))
+
+        for f in range(2, filas + 1):
+            for c in range(1, columnas + 1):
+                valores = self.driver.find_element(By.XPATH, "/html/body/table/tbody/tr[" + str(f) + "]/td[" + str(c) + "]").text
+                self.assertEqual(valores, "usuario2")
+                self.driver.find_element(By.ID, "btnDelete").click()
+                self.driver.find_element(By.ID, "dialogDeleteBtn").click()
+
+        print("US 3 | TC03 - Validar Borrar Nuevo Usuario")
+
+
+    def tearDown(self):
+        self.driver.quit()
 
 
 
