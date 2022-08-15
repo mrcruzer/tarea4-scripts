fc2eb0436c21517b05a11b82b83b1c4d8b6ea1e3
mrcruzer <isaacavelino14@gmail.com>
2022-08-15 14:06:18 -0400

    cualificaciones modificado 1

--- US_4-Cualificaciones.py
+++ US_4-Cualificaciones.py
@@ -51,6 +51,40 @@ class Cualificaciones(unittest.TestCase):
 
                 print(valores).clic()
 
+    def test_admin_education(self):
+        usuario = self.driver.find_element(By.ID, 'txtUsername').send_keys("Admin ")
+        password = self.driver.find_element(By.ID, 'txtPassword').send_keys("admin123")
+        login = self.driver.find_element(By.ID, "btnLogin").click()
+
+        actions = ActionChains(self.driver)
+        menu1 = self.driver.find_element(By.ID, "menu_admin_viewAdminModule")
+        menu2 = self.driver.find_element(By.ID, "menu_admin_Qualifications")
+        menu3 = self.driver.find_element(By.ID, "menu_admin_viewSkills")
+        actions.move_to_element(menu1).move_to_element(menu2).move_to_element(menu3).perform()
+        menu3.click()
+
+        add = self.driver.find_element(By.ID, "btnAdd").click()
+
+        name_skill = self.driver.find_element(By.ID, 'skill_name').send_keys("Skill 1")
+        description_skill = self.driver.find_element(By.ID, 'skill_description').send_keys("Description Skill 1")
+        save_skill = self.driver.find_element(By.ID, "btnSave").click()
+
+        filas = len(self.driver.find_elements(By.XPATH, "//table[@id='recordsListTable']//thead//tr"))
+        columnas = len(self.driver.find_elements(By.XPATH, "//table[@id='recordsListTable']//tbody//tr[1]/td"))
+
+            # print(filas)
+            # print(columnas)
+
+        for f in range(2, filas + 1):
+            print(f)
+
+        for f in range(2, filas + 1):
+            for c in range(1, columnas + 1):
+                valores = self.driver.find_element(By.XPATH, "//table[id='recordsListTable']//tbody//tr[" + str(
+                        f) + "]/td[" + str(c) + "]").text
+
+                print(valores).clic()
+
 
 
 
@@ -58,7 +92,138 @@ class Cualificaciones(unittest.TestCase):
         #creacion = self.driver.find_element(By.XPATH("//table[@id='recordsListTable']//tbody/tr[2]//td"));
         #print(creacion.text)
 
-        print("Prueba Correcta - US 4 | TC01 - Crear, Editar y Borrar Skill")
+        print("Prueba Correcta - US 4 | TC02 - Crear, Editar y Borrar Education")
+
+    def test_admin_license(self):
+        usuario = self.driver.find_element(By.ID, 'txtUsername').send_keys("Admin ")
+        password = self.driver.find_element(By.ID, 'txtPassword').send_keys("admin123")
+        login = self.driver.find_element(By.ID, "btnLogin").click()
+
+        actions = ActionChains(self.driver)
+        menu1 = self.driver.find_element(By.ID, "menu_admin_viewAdminModule")
+        menu2 = self.driver.find_element(By.ID, "menu_admin_Qualifications")
+        menu3 = self.driver.find_element(By.ID, "menu_admin_viewSkills")
+        actions.move_to_element(menu1).move_to_element(menu2).move_to_element(menu3).perform()
+        menu3.click()
+
+        add = self.driver.find_element(By.ID, "btnAdd").click()
+
+        name_skill = self.driver.find_element(By.ID, 'skill_name').send_keys("Skill 1")
+        description_skill = self.driver.find_element(By.ID, 'skill_description').send_keys("Description Skill 1")
+        save_skill = self.driver.find_element(By.ID, "btnSave").click()
+
+        filas = len(self.driver.find_elements(By.XPATH, "//table[@id='recordsListTable']//thead//tr"))
+        columnas = len(self.driver.find_elements(By.XPATH, "//table[@id='recordsListTable']//tbody//tr[1]/td"))
+
+            # print(filas)
+            # print(columnas)
+
+        for f in range(2, filas + 1):
+            print(f)
+
+        for f in range(2, filas + 1):
+            for c in range(1, columnas + 1):
+                valores = self.driver.find_element(By.XPATH, "//table[id='recordsListTable']//tbody//tr[" + str(
+                        f) + "]/td[" + str(c) + "]").text
+
+                print(valores).click()
+
+
+
+
+
+        #creacion = self.driver.find_element(By.XPATH("//table[@id='recordsListTable']//tbody/tr[2]//td"));
+        #print(creacion.text)
+
+        print("Prueba Correcta - US 4 | TC02 - Crear, Editar y Borrar Education")
+
+
+    def test_admin_languages(self):
+        usuario = self.driver.find_element(By.ID, 'txtUsername').send_keys("Admin ")
+        password = self.driver.find_element(By.ID, 'txtPassword').send_keys("admin123")
+        login = self.driver.find_element(By.ID, "btnLogin").click()
+
+        actions = ActionChains(self.driver)
+        menu1 = self.driver.find_element(By.ID, "menu_admin_viewAdminModule")
+        menu2 = self.driver.find_element(By.ID, "menu_admin_Qualifications")
+        menu3 = self.driver.find_element(By.ID, "menu_admin_viewSkills")
+        actions.move_to_element(menu1).move_to_element(menu2).move_to_element(menu3).perform()
+        menu3.click()
+
+        add = self.driver.find_element(By.ID, "btnAdd").click()
+
+        name_skill = self.driver.find_element(By.ID, 'skill_name').send_keys("Skill 1")
+        description_skill = self.driver.find_element(By.ID, 'skill_description').send_keys("Description Skill 1")
+        save_skill = self.driver.find_element(By.ID, "btnSave").click()
+
+        filas = len(self.driver.find_elements(By.XPATH, "//table[@id='recordsListTable']//thead//tr"))
+        columnas = len(self.driver.find_elements(By.XPATH, "//table[@id='recordsListTable']//tbody//tr[1]/td"))
+
+            # print(filas)
+            # print(columnas)
+
+        for f in range(2, filas + 1):
+            print(f)
+
+        for f in range(2, filas + 1):
+            for c in range(1, columnas + 1):
+                valores = self.driver.find_element(By.XPATH, "//table[id='recordsListTable']//tbody//tr[" + str(
+                        f) + "]/td[" + str(c) + "]").text
+
+                print(valores).clic()
+
+
+
+
+
+        #creacion = self.driver.find_element(By.XPATH("//table[@id='recordsListTable']//tbody/tr[2]//td"));
+        #print(creacion.text)
+
+        print("Prueba Correcta - US 4 | TC02 - Crear, Editar y Borrar Education")
+
+
+    def test_admin_membership(self):
+        usuario = self.driver.find_element(By.ID, 'txtUsername').send_keys("Admin ")
+        password = self.driver.find_element(By.ID, 'txtPassword').send_keys("admin123")
+        login = self.driver.find_element(By.ID, "btnLogin").click()
+
+        actions = ActionChains(self.driver)
+        menu1 = self.driver.find_element(By.ID, "menu_admin_viewAdminModule")
+        menu2 = self.driver.find_element(By.ID, "menu_admin_Qualifications")
+        menu3 = self.driver.find_element(By.ID, "menu_admin_viewSkills")
+        actions.move_to_element(menu1).move_to_element(menu2).move_to_element(menu3).perform()
+        menu3.click()
+
+        add = self.driver.find_element(By.ID, "btnAdd").click()
+
+        name_skill = self.driver.find_element(By.ID, 'skill_name').send_keys("Skill 1")
+        description_skill = self.driver.find_element(By.ID, 'skill_description').send_keys("Description Skill 1")
+        save_skill = self.driver.find_element(By.ID, "btnSave").click()
+
+        filas = len(self.driver.find_elements(By.XPATH, "//table[@id='recordsListTable']//thead//tr"))
+        columnas = len(self.driver.find_elements(By.XPATH, "//table[@id='recordsListTable']//tbody//tr[1]/td"))
+
+            # print(filas)
+            # print(columnas)
+
+        for f in range(2, filas + 1):
+            print(f)
+
+        for f in range(2, filas + 1):
+            for c in range(1, columnas + 1):
+                valores = self.driver.find_element(By.XPATH, "//table[id='recordsListTable']//tbody//tr[" + str(
+                        f) + "]/td[" + str(c) + "]").text
+
+                print(valores).clic()
+
+
+
+
+
+        #creacion = self.driver.find_element(By.XPATH("//table[@id='recordsListTable']//tbody/tr[2]//td"));
+        #print(creacion.text)
+
+        print("Prueba Correcta - US 4 | TC02 - Crear, Editar y Borrar Education")
 
 
 
